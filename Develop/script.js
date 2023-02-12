ae9a8d9932e8285bd82949418ff88ea89300373f
William Combee <william.combee@yahoo.com>
2023-02-11 23:16:32 -0600

    added true if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 var characterLength = 8;
-var choice = [];
+var options = [];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var specialChar = ["!","@","#","$","%","^","&","*","?","<",">","/"];
@@ -12,8 +12,8 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var password = "";
   for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choice.length);
-    password = password + choice[randomIndex];
+    var randomIndex = Math.floor(Math.random() * options.length);
+    password = password + options[Index];
 
   }
   return password;
@@ -21,7 +21,7 @@ function generatePassword() {
 
 // Prompt the user for the password criteria
 function getPrompts() {
-  choice = [];
+  options = [];
 
   characterLength = parseInt(prompt("How many characters would you like your password to be? 8 - 128"));
 
@@ -31,8 +31,21 @@ function getPrompts() {
     return false;
   }
 
-  //if choices meet criteria (true)
-  
+  //if options meet criteria (true) THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters 
+  if(confirm("Would you like your password to contain lowercase letters?")){
+    options = options.concat(lowerCase);
+  }
+  if(confirm("Would you like your password to contain UPPERCASE letters?")){
+    options = options.concat(upperCase);
+  }
+  if(confirm("Would you like your password to contain numbers?")){
+    options = options.concat(numbers);
+  }
+  if(confirm("Would you like your password to contain special characters?")){
+    options = options.concat(specialChar);
+  }
+
+  return true;
 }
 
 // Write password to the #password input
